b64e77a182bacb0246032f4d4c64aca5b09234d7
Nikolas-as <nikkolas21082003@gmail.com>
2021-09-12 14:59:04 -0300

    updates :construction:

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,31 +3,56 @@ import { Link } from "react-router-dom"
 import './styles.scss'
 
 export function Header () {
+  window.onload=function(){
+    let show = true;
+
+    const menuSection = document.querySelector(".menu-section");
+    const menuToggle = document.querySelector(".menu-toggle");
+    
+      menuToggle.addEventListener("click", () => {
+
+        document.body.style.overflow = show ? "hidden" : "initial";
+
+        menuSection.classList.toggle("on", show);
+        show = !show;
+      })
+}
+
   return (
     <main className="Navbar">
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
+        <div class="menu-section">
+          <div class="menu-toggle">
+            <div class="one"></div>
+            <div class="two"></div>
+            <div class="three"></div>
+          </div>
+          <nav>
+            <ul>
+              <Link to="/">
+                <li>Home</li>
+              </Link>
 
-        <Link to="aboutme">
-          <li>Sobre mim</li>
-        </Link>
+              <Link to="aboutme">
+                <li>Sobre mim</li>
+              </Link>
 
-        <Link to="skills">
-          <li>Skills</li>
-        </Link>
+              <Link to="skills">
+                <li>Skills</li>
+              </Link>      
 
-        <Link to="projects">
-          <li>Projetos</li>
-        </Link>
+              <Link to="projects">
+                <li>Projetos</li>
+              </Link>
 
-        <a href="https://api.whatsapp.com/send?phone=5511969727693">
-        <button>
-        Contato
-        </button>
-      </a>  
-      </ul>
+              <a href="https://api.whatsapp.com/send?phone=5511969727693">
+                <button>
+                Contato
+                </button>
+              </a>  
+
+            </ul>
+          </nav>
+        </div>
     </main>
   );
 }
