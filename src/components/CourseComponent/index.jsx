5b96325766012e72e25787314abe898ac265d894
Nikolas-as <nikkolas21082003@gmail.com>
2021-11-08 22:30:37 -0300

    feat: :sparkles: ading .map and axios

--- src/components/CourseComponent/index.jsx
+++ src/components/CourseComponent/index.jsx
@@ -1,53 +1,43 @@
+import { useState, useEffect } from 'react';
+
 import rocketseatImg from '../../assets/images/Rocketseat.png';
+
+import axios from 'axios'
+
 import './styles.scss'
 
 export function CourseComponent() {
+  const [ courses, setCourses ] = useState([]);
+
+  const api = axios.create({
+    baseURL: "https://flashcard-api-mayck.herokuapp.com/api/colecoes"
+  });
+
+  useEffect(() => {
+    fetch('https://flashcard-api-mayck.herokuapp.com/api/colecoes') 
+      .then(response => response.json())                                                                       
+      .then(data => setCourses(data))
+
+    api.post('/', {
+      nome: "D.A.W",
+      descricao: "É a melhor matéria de todas :)"
+    })
+  }, [])
+
   return (
-    <>
       <div className="container">
 
         <h2>Cursos</h2>
 
         <div className="content-card">
+        {courses.map(course => (
           <div className="card">
-            <span>Curso GoStack Rocketseat</span>
+            <span>{ course.nome }</span>
             <img src={rocketseatImg} alt="rocketseatImg" />
+            <p>{ course.descricao }</p>
           </div>
-          <div className="card">
-            <span>Curso GoStack Rocketseat</span>
-            <img src={rocketseatImg} alt="rocketseatImg" />
-          </div>
-          <div className="card">
-            <span>Curso GoStack Rocketseat</span>
-            <img src={rocketseatImg} alt="rocketseatImg" />
-          </div>
-           <div className="card">
-            <span>Curso GoStack Rocketseat</span>
-            <img src={rocketseatImg} alt="rocketseatImg" />
-          </div>
-          <div className="card">
-            <span>Curso GoStack Rocketseat</span>
-            <img src={rocketseatImg} alt="rocketseatImg" />
-          </div>
-          <div className="card">
-            <span>Curso GoStack Rocketseat</span>
-            <img src={rocketseatImg} alt="rocketseatImg" />
-          </div>
-          {/*
-          <div className="card">
-            <span>FlexBlog</span>
-            <img src={FlexBlog} alt="flexblog" />
-          </div>
-          <div className="card">
-            <span>Task</span>
-            <img src={Task} alt="task" />
-          </div>
-          <div className="card">
-            <span>RocketShoes</span>
-            <img src={RocketShoes} alt="rocketshoes" />
-          </div> */}
+        ))}
         </div>
       </div>
-    </>
   );
 }
